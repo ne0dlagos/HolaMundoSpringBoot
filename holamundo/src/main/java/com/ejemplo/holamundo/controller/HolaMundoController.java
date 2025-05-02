752fbc75599fb072bcdd6de7baffef123d973e4d
JoaquinCortesP <164568621+JoaquinCortesP@users.noreply.github.com>
2025-05-02 16:30:57 -0400

    Add files via upload

---
+++ holamundo/src/main/java/com/ejemplo/holamundo/controller/HolaMundoController.java
@@ -0,0 +1,18 @@
+package com.ejemplo.holamundo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+        return "Hola Mundo";
+
+
+    }
+}
